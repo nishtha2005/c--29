7cfbf89e7104b8d7e26fd5d8de9dd7e8a08bb9db
nishtha2005 <nishthashrivas62@gmail.com>
2021-05-27 16:51:37 +0530

    quicker

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,9 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.s1=loadImage("sprites/sling1.png")
+        this.s2=loadImage("sprites/sling2.png")
+        this.s3=loadImage("sprites/sling3.png")
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +19,30 @@ class SlingShot{
     }
 
     display(){
+        image(this.s1,200,20)
+        image(this.s2,170,20)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y)
+                image(this.s3,pointA.x-30,pointA.y-10,25,20)
+
+
+            }
+            else{
+                strokeWeight(7);
+                line(pointA.x+20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+30, pointB.y)
+                image(this.s3,pointA.x+25,pointA.y-10,25,20)
+            }
+            
+           
+            stroke(48,22,8)
+           
+
         }
     }
     
